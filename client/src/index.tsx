f832722ca61d6172a739b43288e29479c86ec022
hommat <zmateusz1111@gmail.com>
2020-06-03 14:07:29 +0200

    Make PWA

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import checkEnvVars from "./checkEnvVars";
+import { register } from "./serviceWorker";
 
 checkEnvVars();
 
@@ -12,3 +13,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+register();
